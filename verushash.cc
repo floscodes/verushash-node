d5f427919029c9c5675f5f4abc7641b0cf38d1f3
flopetautschnig (floscodes) <mail@floscodes.net>
2026-01-27 16:57:36 +0100

    style: refactor verushash.cc

--- verushash.cc
+++ verushash.cc
@@ -2,9 +2,7 @@
 #include <mutex>
 #include "crypto/verus_hash.h"
 
-extern "C" {
-
-bool initialized = false;
+static bool initialized = false;
 
 CVerusHash* vh = nullptr;
 CVerusHashV2* vh2 = nullptr;
@@ -16,6 +14,9 @@ static std::once_flag init_flag;
 static void initialize()
 {
     std::call_once(init_flag, []() {
+
+        if (initialized) return;
+
         CVerusHash::init();
         CVerusHashV2::init();
 
@@ -23,8 +24,11 @@ static void initialize()
         vh2  = new CVerusHashV2(SOLUTION_VERUSHHASH_V2);
         vh2b1 = new CVerusHashV2(SOLUTION_VERUSHHASH_V2_1);
         vh2b2 = new CVerusHashV2(SOLUTION_VERUSHHASH_V2_2);
+        initialized = true;
     });
 }
+
+extern "C" {
 
 void verus_v1_hash(void* result, const void* data, size_t len) {
     initialize();
@@ -50,6 +54,6 @@ void verus_v2_2_hash(void* result, const void* data, size_t len) {
     vh2b2->Reset();
     vh2b2->Write(reinterpret_cast<const unsigned char*>(data), len);
     vh2b2->Finalize2b(reinterpret_cast<unsigned char*>(result));
-}
 
+}
 }
