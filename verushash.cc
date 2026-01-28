5e185de24fd10640b62d9c0b1e4ce420ab829e09
flopetautschnig (floscodes) <mail@floscodes.net>
2026-01-28 13:13:08 +0100

    style: let rust manage the memory

--- verushash.cc
+++ verushash.cc
@@ -1,50 +1,46 @@
 #include <stdint.h>
+#include <new>
 #include "crypto/verus_hash.h"
 
-// Einmalige Initialisierung der globalen CPU-Konstanten
-static void global_setup() {
-    static bool global_initialized = false;
-    if (!global_initialized) {
-        CVerusHash::init();
-        CVerusHashV2::init();
-        global_initialized = true;
-    }
+// Einmalige Initialisierung der Hardware-Optimierungen
+extern "C" void verus_hash_init_all() {
+    CVerusHash::init();
+    CVerusHashV2::init();
 }
 
 extern "C" {
     __attribute__((force_align_arg_pointer))
-    void verus_v1_hash(void* result, const void* data, size_t len) {
-        global_setup();
-        // Objekt auf dem Stack statt auf dem Heap
-        alignas(32) CVerusHash vh; 
-        vh.Write(reinterpret_cast<const unsigned char*>(data), len);
-        vh.Finalize(reinterpret_cast<unsigned char*>(result));
+    void verus_v1_hash(void* result, const void* data, size_t len, void* obj_space) {
+        CVerusHash* vh = new (obj_space) CVerusHash();
+        vh->Write(reinterpret_cast<const unsigned char*>(data), len);
+        vh->Finalize(reinterpret_cast<unsigned char*>(result));
+        vh->~CVerusHash();
     }
 
     __attribute__((force_align_arg_pointer))
-    void verus_v2_hash(void* result, const void* data, size_t len) {
-        global_setup();
-        alignas(32) CVerusHashV2 vh2(SOLUTION_VERUSHHASH_V2);
-        vh2.Reset();
-        vh2.Write(reinterpret_cast<const unsigned char*>(data), len);
-        vh2.Finalize(reinterpret_cast<unsigned char*>(result));
+    void verus_v2_hash(void* result, const void* data, size_t len, void* obj_space) {
+        CVerusHashV2* vh = new (obj_space) CVerusHashV2(SOLUTION_VERUSHHASH_V2);
+        vh->Reset();
+        vh->Write(reinterpret_cast<const unsigned char*>(data), len);
+        vh->Finalize(reinterpret_cast<unsigned char*>(result));
+        vh->~CVerusHashV2();
     }
 
     __attribute__((force_align_arg_pointer))
-    void verus_v2_1_hash(void* result, const void* data, size_t len) {
-        global_setup();
-        alignas(32) CVerusHashV2 vh2b1(SOLUTION_VERUSHHASH_V2_1);
-        vh2b1.Reset();
-        vh2b1.Write(reinterpret_cast<const unsigned char*>(data), len);
-        vh2b1.Finalize2b(reinterpret_cast<unsigned char*>(result));
+    void verus_v2_1_hash(void* result, const void* data, size_t len, void* obj_space) {
+        CVerusHashV2* vh = new (obj_space) CVerusHashV2(SOLUTION_VERUSHHASH_V2_1);
+        vh->Reset();
+        vh->Write(reinterpret_cast<const unsigned char*>(data), len);
+        vh->Finalize2b(reinterpret_cast<unsigned char*>(result));
+        vh->~CVerusHashV2();
     }
 
     __attribute__((force_align_arg_pointer))
-    void verus_v2_2_hash(void* result, const void* data, size_t len) {
-        global_setup();
-        alignas(32) CVerusHashV2 vh2b2(SOLUTION_VERUSHHASH_V2_2);
-        vh2b2.Reset();
-        vh2b2.Write(reinterpret_cast<const unsigned char*>(data), len);
-        vh2b2.Finalize2b(reinterpret_cast<unsigned char*>(result));
+    void verus_v2_2_hash(void* result, const void* data, size_t len, void* obj_space) {
+        CVerusHashV2* vh = new (obj_space) CVerusHashV2(SOLUTION_VERUSHHASH_V2_2);
+        vh->Reset();
+        vh->Write(reinterpret_cast<const unsigned char*>(data), len);
+        vh->Finalize2b(reinterpret_cast<unsigned char*>(result));
+        vh->~CVerusHashV2();
     }
 }
